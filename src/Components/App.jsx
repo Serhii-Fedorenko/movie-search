9ef99d14d4cb97e115895cf33a12e1b0114b1a3a
SerhiiFedorenko <serhii.fedorenko90@gmail.com>
2024-02-11 20:03:59 +0200

    fetch on home page

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -4,18 +4,25 @@ import Movies from "../Pages/Movies";
 import NotFound from "../Pages/NotFound";
 import MovieDetails from "./MovieDetails";
 import SharedLayout from "./SharedLayout";
+import axios from "axios";
+
+axios.defaults.baseURL = "https://api.themoviedb.org/3";
+axios.defaults.headers.common["Authorization"] =
+  "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYjU5Mjk2ODZlYTRmM2FkMGQyZjdhMzI0NGZiZTEyMiIsInN1YiI6IjY1M2NkZDc3NTE5YmJiMDBlMThjNDMyOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XMEVEZj4LIB64XBgIkKb9CzaDHet9ZxU3is9P1Tg5Zc";
 
 function App() {
-  return <>
-    <Routes>
-      <Route path="/" element={<SharedLayout />}>
-        <Route index element={<Home />}></Route>
-        <Route path="movies" element={<Movies />}></Route>
-        <Route path="movies/:movieId" element={<MovieDetails />}></Route>
-      </Route>
-      <Route path="*" element={<NotFound />}></Route>
-    </Routes>
-  </>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home />}></Route>
+          <Route path="movies" element={<Movies />}></Route>
+          <Route path="movies/:movieId" element={<MovieDetails />}></Route>
+        </Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,26 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+
 const Home = () => {
-  return <div>HomePage</div>;
+  const [collection, setCollection] = useState([]);
+  useEffect(() => {
+    axios
+      .get("/trending/movie/day?language=en-US")
+      .then(({ data }) => setCollection(data.results))
+      .then(console.log(collection));
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {collection?.map((movie) => (
+          <li key={movie.id}><Link to={`/movies/${movie.id}`}>{movie.original_title}</Link></li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
