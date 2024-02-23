de206a069deb70339a02bc6e22c2c5599233a0d6
SerhiiFedorenko <serhii.fedorenko90@gmail.com>
2024-02-23 20:05:23 +0200

    base styles for header

--- src/Components/SharedLayout.jsx
+++ src/Components/SharedLayout.jsx
@@ -1,17 +1,22 @@
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { Suspense } from "react";
+import { Container, Header, Link } from "./SharedLayout.styled";
 
 const SharedLayout = () => {
   return (
-    <>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/movies">Movies</Link>
-      </nav>
-      <Suspense fallback={<div>Loading...</div>}>
+    <Container>
+      <Header>
+        <nav>
+          <Link to="/" end>
+            Home
+          </Link>
+          <Link to="/movies">Movies</Link>
+        </nav>
+      </Header>
+      <Suspense fallback={<div>Loading page...</div>}>
         <Outlet />
       </Suspense>
-    </>
+    </Container>
   );
 };
 
