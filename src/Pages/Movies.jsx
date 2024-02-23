2341dc2db0d6735487201d96d0451fcb55824b58
SerhiiFedorenko <serhii.fedorenko90@gmail.com>
2024-02-23 19:35:14 +0200

    add style-components lib

--- src/Pages/Movies.jsx
+++ src/Pages/Movies.jsx
@@ -9,7 +9,7 @@ const Movies = () => {
   const location = useLocation();
 
   const updateQueryString = (e) => {
-    setSearchParams(e.target.value !== '' ? {query: e.target.value} : {});
+    setSearchParams(e.target.value !== "" ? { query: e.target.value } : {});
   };
 
   const fetchMovies = (query) => {
@@ -34,8 +34,8 @@ const Movies = () => {
   }, []);
 
   const sortedMovies = (collection) => {
-    return collection.sort((a,b) => b.vote_count - a.vote_count)
-  }
+    return collection.sort((a, b) => b.popularity - a.popularity);
+  };
 
   return (
     <div>
@@ -46,7 +46,7 @@ const Movies = () => {
       <ul>
         {sortedMovies(collection)?.map((movie) => (
           <li key={movie.id}>
-            <Link to={`${movie.id}`} state={{from: location}}>
+            <Link to={`${movie.id}`} state={{ from: location }}>
               <h3>{movie.title}</h3>
               <img
                 src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
