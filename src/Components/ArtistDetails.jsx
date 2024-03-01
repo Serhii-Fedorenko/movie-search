6e138d08d0ff5678555530f3dc3781718b940604
SerhiiFedorenko <serhii.fedorenko90@gmail.com>
2024-03-01 18:46:45 +0200

    made artist page without styles

--- src/Components/ArtistDetails.jsx
+++ src/Components/ArtistDetails.jsx
@@ -6,19 +6,32 @@ import { useLocation, useParams } from "react-router-dom";
 const ArtistDetails = () => {
   const [artist, setArtist] = useState(null);
   const { artistName } = useParams();
-//   const location = useLocation();
-//   const backLinkHref = useRef(location.state?.from ?? "/movie");
+  //   const location = useLocation();
+  //   const backLinkHref = useRef(location.state?.from ?? "/movie");
 
   useEffect(() => {
     axios
       .get(`/search/person?query=${artistName}`)
-      .then(({data}) => setArtist(data.results[0]))
+      .then(({ data }) => setArtist(data.results[0]))
       .catch((error) => console.log(error));
   }, [artistName]);
 
-  console.log(artist)
+  console.log(artist);
 
-  return <div>sdlfskdghsl</div>;
+  return (
+    <div>
+      {artist && (
+        <div>
+          <h4>{artist.name}</h4>
+          <img
+            src={`https://image.tmdb.org/t/p/w300/${artist.profile_path}`}
+            alt={artist.name}
+          />
+          <ul>{artist.known_for.map(item => <li>{item.title}</li>)}</ul>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default ArtistDetails;
