b8d50ee323c9ceac0825e869f77a76a3a9e90cf1
SerhiiFedorenko <serhii.fedorenko90@gmail.com>
2024-03-06 19:20:40 +0200

    separated MovieDetails page by components

---
+++ src/Components/MovieCardContainer.jsx
@@ -0,0 +1,48 @@
+import { Button, CardWrap, LeftSide, RightSide } from "./MovieCard.styled";
+
+const MovieCardContainer = ({ movie, backLinkHref }) => {
+  return (
+    <>
+      <Button to={backLinkHref.current}>Back to previous page</Button>
+      <CardWrap>
+        <LeftSide>
+          <h2>{movie.title}</h2>
+          <img
+            src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+            alt={movie.original_title}
+            width="300"
+          />
+        </LeftSide>
+        <RightSide>
+          <h4>{movie.tagline}</h4>
+          <p>{movie.overview}</p>
+          <img
+            src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`}
+            alt={movie.original_title}
+            width="500"
+          />
+          <p>Release Date: {movie.release_date}</p>
+          <p>Genres</p>
+          <ul>
+            {movie.genres.map((genre) => (
+              <li key={genre.name}>{genre.name}</li>
+            ))}
+          </ul>
+          <p>Budget: ${movie.budget}</p>
+          <ul>
+            {movie.production_countries.map((country) => (
+              <li key={country.name}>{country.name}</li>
+            ))}
+          </ul>
+          <p>{movie.runtime} min</p>
+        </RightSide>
+      </CardWrap>
+      <nav>
+        <Button to="cast">Cast</Button>
+        <Button to="reviews">Reviews</Button>
+      </nav>
+    </>
+  );
+};
+
+export default MovieCardContainer;
--- src/Components/MovieDetails.jsx
+++ src/Components/MovieDetails.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState, Suspense, useRef } from "react";
 import { useParams, Outlet, useLocation } from "react-router-dom";
 import axios from "axios";
-import { CardWrap, Button, LeftSide, RightSide } from "./MovieCard.styled";
+import MovieCardContainer from "./MovieCardContainer";
 
 const MovieDetails = () => {
   const [movie, setMovie] = useState(null);
@@ -15,46 +15,9 @@ const MovieDetails = () => {
 
   return (
     <div>
-      <Button to={backLinkHref.current}>Back to previous page</Button>
       {movie && (
-        <CardWrap>
-          <LeftSide>
-            <h2>{movie.title}</h2>
-            <img
-              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-              alt={movie.original_title}
-              width="300"
-            />
-          </LeftSide>
-          <RightSide>
-            <h4>{movie.tagline}</h4>
-            <p>{movie.overview}</p>
-            <img
-              src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`}
-              alt={movie.original_title}
-              width="500"
-            />
-            <p>Release Date: {movie.release_date}</p>
-            <p>Genres</p>
-            <ul>
-              {movie.genres.map((genre) => (
-                <li key={genre.name}>{genre.name}</li>
-              ))}
-            </ul>
-            <p>Budget: ${movie.budget}</p>
-            <ul>
-              {movie.production_countries.map((country) => (
-                <li key={country.name}>{country.name}</li>
-              ))}
-            </ul>
-            <p>{movie.runtime} min</p>
-          </RightSide>
-        </CardWrap>
+        <MovieCardContainer movie={movie} backLinkHref={backLinkHref} />
       )}
-      <nav>
-        <Button to="cast">Cast</Button>
-        <Button to="reviews">Reviews</Button>
-      </nav>
       <Suspense fallback={<div>Loading...</div>}>
         <Outlet />
       </Suspense>
