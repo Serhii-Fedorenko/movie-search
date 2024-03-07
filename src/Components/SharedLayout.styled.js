7e67e3399e3dffce9e8f8174b8e5531669389730
SerhiiFedorenko <serhii.fedorenko90@gmail.com>
2024-03-07 19:36:55 +0200

    fixed Header nav on top of the page

--- src/Components/SharedLayout.styled.js
+++ src/Components/SharedLayout.styled.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 
 export const Container = styled.div`
+  position: relative;
   max-width: 1200px;
   margin: 0 auto;
   padding: 0 16px;
@@ -9,12 +10,17 @@ export const Container = styled.div`
 
 export const Header = styled.header`
   display: flex;
+  position: sticky;
+  right: 0;
+  left: 0;
+  top: 0;
   align-items: center;
   justify-content: space-between;
   gap: 12px;
   padding: 8px 0;
   margin-bottom: 16px;
   border-bottom: 1px solid black;
+  background-color: white;
 
   > nav {
     display: flex;
